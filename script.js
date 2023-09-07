04bdbbf24f2fd13dba8522f6f8647ca4d8fc0ca9
gearprn <gearprn@gmail.com>
2023-09-07 13:49:01 +0700

    feat: implementing web speech api

---
+++ script.js
@@ -0,0 +1,49 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent
+
+const words = ['หน้า', 'ผ้าไหม', 'วัด', 'มะลิ', 'สีแดง']
+const grammar = `#JSGF V1.0; grammar colors; public <color> = ${words.join(
+  ' | '
+)};`
+
+const recognition = new SpeechRecognition()
+const speechRecognitionList = new SpeechGrammarList()
+speechRecognitionList.addFromString(grammar, 1)
+
+recognition.grammars = speechRecognitionList
+recognition.continuous = false
+recognition.lang = 'th-TH'
+recognition.interimResults = false
+recognition.maxAlternatives = 1
+
+const startBtn = document.getElementById('start-btn')
+const diagnostic = document.querySelector('.output')
+const confidence = document.querySelector('.confidence')
+
+startBtn.onclick = () => {
+  recognition.start()
+  console.log('Ready to receive a color command.')
+}
+
+recognition.onresult = (event) => {
+  const word = event.results[0][0].transcript
+  diagnostic.textContent = `Result received: ${word}.`
+  confidence.textContent = `Confidence: ${event.results[0][0].confidence}.`
+  console.log(`Confidence: ${event.results[0][0].confidence}`)
+}
+
+recognition.onspeechend = () => {
+  recognition.stop()
+}
+
+recognition.onnomatch = (event) => {
+  diagnostic.textContent = "I didn't recognize that word."
+}
+
+recognition.onerror = (event) => {
+  diagnostic.textContent = `Error occurred in recognition: ${event.error}`
+}
